8129f8386f1335c7888069d99255ed91ca2d8d21
Boris Krit <scriptboris@gmail.com>
2018-05-18 22:49:40 +0800

    init

---
+++ UnityServer/Server.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class Server
+    {
+        private bool _run;
+
+        #region private members 	
+        /// <summary> 	
+        /// TCPListener to listen for incomming TCP connection 	
+        /// requests. 	
+        /// </summary> 	
+        private TcpListener tcpListener;
+        /// <summary> 
+        /// Background thread for TcpServer workload. 	
+        /// </summary> 	
+        private Thread tcpListenerThread;
+        /// <summary> 	
+        /// Create handle to connected tcp client. 	
+        /// </summary> 	
+        private TcpClient connectedTcpClient;
+        #endregion
+
+        public Server()
+        {
+            tcpListenerThread = new Thread(new ThreadStart(ListenForIncommingRequests));
+            tcpListenerThread.IsBackground = true;
+            tcpListenerThread.Start();
+        }
+
+        void Loop()
+        {
+            while (_run)
+            {
+                SendMessage();
+                Thread.Sleep(18);
+            }
+        }
+
+        /// <summary> 	
+        /// Runs in background TcpServerThread; Handles incomming TcpClient requests 	
+        /// </summary> 	
+        private void ListenForIncommingRequests()
+        {
+            try
+            {
+                // Create listener on localhost port 8052. 			
+                tcpListener = new TcpListener(IPAddress.Parse("127.0.0.1"), 8000);
+                tcpListener.Start();
+                Byte[] bytes = new Byte[1024];
+                while (true)
+                {
+                    using (connectedTcpClient = tcpListener.AcceptTcpClient())
+                    {
+                        // Get a stream object for reading 					
+                        using (var stream = connectedTcpClient.GetStream())
+                        {
+                            int length;
+                            // Read incomming stream into byte arrary. 						
+                            while ((length = stream.Read(bytes, 0, bytes.Length)) != 0)
+                            {
+                                var incommingData = new byte[length];
+                                Array.Copy(bytes, 0, incommingData, 0, length);
+                                // Convert byte array to string message. 							
+                                string clientMessage = Encoding.ASCII.GetString(incommingData);
+                                Console.WriteLine("client message received as: " + clientMessage);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SocketException socketException)
+            {
+                Console.Write("SocketException " + socketException.ToString());
+            }
+        }
+        /// <summary> 	
+        /// Send message to client using socket connection. 	
+        /// </summary> 	
+        private void SendMessage()
+        {
+            if (connectedTcpClient == null)
+            {
+                return;
+            }
+
+            try
+            {
+                // Get a stream object for writing. 			
+                NetworkStream stream = connectedTcpClient.GetStream();
+                if (stream.CanWrite)
+                {
+                    string serverMessage = "This is a message from your server.";
+                    // Convert string message to byte array.                 
+                    byte[] serverMessageAsByteArray = Encoding.ASCII.GetBytes(serverMessage);
+                    // Write byte array to socketConnection stream.               
+                    stream.Write(serverMessageAsByteArray, 0, serverMessageAsByteArray.Length);
+                    Console.WriteLine("Server sent his message - should be received by client");
+                }
+            }
+            catch (SocketException socketException)
+            {
+                Console.WriteLine("Socket exception: " + socketException);
+            }
+        }
+    }
+}
